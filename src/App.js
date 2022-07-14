5a36bc6e951f188b15de112ae60042a9950ceb0e
jay-ae <jaygajjjar143@gmail.com>
2022-07-14 11:52:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route, Switch } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { Switch } from 'react-router-dom'
 import Navigation from "./components/Header/Navigation";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home";
@@ -20,15 +21,15 @@ function App() {
 
       <Navigation />
       <ScrollToTop>
-        <Routes>
-          <Switch>
-            <Route exact path='/' element={<Home />}></Route>
-            <Route exact path='/services' element={<Services />}></Route>
-            <Route exact path='/galary' element={<Galary />}></Route>
-            <Route exact path='/about' element={<About />}></Route>
-            <Route path="*" element={<PageNotFound />} ></Route>
-          </Switch>
-        </Routes>
+
+        <Switch>
+          <Route exact path='/' element={<Home />}></Route>
+          <Route exact path='/services' element={<Services />}></Route>
+          <Route exact path='/galary' element={<Galary />}></Route>
+          <Route exact path='/about' element={<About />}></Route>
+          <Route path="*" element={<PageNotFound />} ></Route>
+        </Switch>
+
       </ScrollToTop>
       <Footer />
 
